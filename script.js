6b8a4404d3a969f4ca140619cc52462472e53e85
Jessiquinterot <152110298+Jessiquinterot@users.noreply.github.com>
2026-01-09 14:47:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+fetch('promos.json')
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById('promos');
+
+    data.promos.forEach(promo => {
+      const card = document.createElement('div');
+      card.className = 'card';
+
+      card.innerHTML = `
+        <h3>${promo.nombre}</h3>
+        <p>${promo.descripcion}</p>
+        <div class="price">$ ${promo.precio.toLocaleString()}</div>
+      `;
+
+      container.appendChild(card);
+    });
+  });
 
