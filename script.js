c4c116ea2257b7013b193d3d12bcf7a68d5b37b1
Jessiquinterot <152110298+Jessiquinterot@users.noreply.github.com>
2026-01-09 15:29:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 fetch('promos.json')
-  .then(res => res.json())
+  .then(res => {
+    if (!res.ok) throw new Error('No se pudo cargar promos.json');
+    return res.json();
+  })
   .then(data => {
     const container = document.getElementById('promos');
+    container.innerHTML = '';
 
-    data.promos.forEach(promo => {
+    data.promos.forEach(item => {
       const card = document.createElement('div');
       card.className = 'card';
 
+      const precio = Number(item.precio || 0).toLocaleString('es-AR');
+
       card.innerHTML = `
-        <h3>${promo.nombre}</h3>
-        <p>${promo.descripcion}</p>
-        <div class="price">$ ${promo.precio.toLocaleString()}</div>
+        <h3>${item.nombre}</h3>
+        <p>${item.descripcion}</p>
+        <div class="price">$ ${precio}</div>
       `;
 
       container.appendChild(card);
     });
+  })
+  .catch(err => {
+    const container = document.getElementById('promos');
+    container.innerHTML = `<div class="card"><h3>Error</h3><p>${err.message}</p></div>`;
   });
 
